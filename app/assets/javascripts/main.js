8c47a219c072afc18c86775ba2ef583299a2bf5d
Becky Green <ggggbbybby@gmail.com>
2014-10-26 13:54:17 -0700

    using js instead of coffee, for now

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,25 @@
+main.js(function() {
+  this.draft_preview = angular.module('draftPreview', ['ngRoute', 'ngResource']);
+
+  this.draft_preview.config([
+    '$routeProvider', function($routeProvider) {
+      return $routeProvider.when('/drafts', {
+        templateUrl: '../templates/drafts/index.html',
+        controller: 'DraftIndexController'
+      }).when('/drafts/:id', {
+        templateUrl: '../templates/drafts/show.html',
+        controller: 'DraftShowController'
+      }).otherwise({
+        templateUrl: '../templates/drafts/index.html',
+        controller: 'DraftIndexController'
+      });
+    }
+  ]);
+
+
+  this.draft_preview.factory('Draft', 
+  	function($resource) {
+		return $resource('/api/drafts/:id')
+  });
+
+}).call(this);
